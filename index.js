2252c65ad7bddaeb9b35df42d5e34f68a7ebff31
mbron770 <mbronfin.tsd@gmail.com>
2023-05-28 22:59:46 -0400

    add fetch POST method

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users'
+    const nameAndEmail = {
+        name: name, 
+        email: email,
+    }
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json', 
+        }, 
+
+        body: JSON.stringify(nameAndEmail), 
+    }).then(response => response.json())
+    .then(newUserData => {
+        const header = document.createElement('h2')
+        const id = newUserData.id
+        header.textContent = `Just Created User # ${id}!`
+        document.body.append(header)
+
+    }).catch(error => {
+        const errorHeader = document.createElement('h2')
+        const id = error.message 
+        errorHeader.textContent = `This is the error message: ${id}`
+        document.body.append(errorHeader)
+    })
+}
+        
+
+
+  
